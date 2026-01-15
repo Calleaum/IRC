55311a7085be3e0fc54742ed768a90d5803468b8
Clement <clement1alleaume@gmail.com>
2026-01-15 08:15:28 +0100

    Add files via upload

--- incs/IrcNumericReplies.hpp
+++ incs/IrcNumericReplies.hpp
@@ -45,6 +45,24 @@ const std::string ERR_UMODEUNKNOWNFLAG_MSG = " :Unknown MODE flag\r\n";
 
 const std::string ERR_USERSDONTMATCH = "502";
 const std::string ERR_USERSDONTMATCH_MSG = " :Can't change mode for other users\r\n";
+const std::string ERR_NEEDMOREPARAMS = "461";
+const std::string ERR_ALREADYREGISTRED = "462";
+const std::string ERR_PASSWDMISMATCH = "464";
+const std::string ERR_NONICKNAMEGIVEN = "431";
+const std::string ERR_ERRONEUSNICKNAME = "432";
+const std::string ERR_NICKNAMEINUSE = "433";
+const std::string ERR_USERNOTINCHANNEL = "441";
+const std::string ERR_NOTONCHANNEL = "442";
+const std::string ERR_USERONCHANNEL = "443";
+const std::string ERR_INVITEONLYCHAN = "473";
+const std::string ERR_BADCHANNELKEY = "475";
+const std::string ERR_CHANNELISFULL = "471";
+const std::string ERR_BADCHANMASK = "476";
+const std::string ERR_UNKNOWNMODE = "472";
+const std::string ERR_UNKNOWNCOMMAND = "421";
+const std::string ERR_NOTREGISTERED = "451";
+const std::string ERR_INVALIDCAPCMD = "410";
+const std::string ERR_ERRONEUSUSERNAME = "468";
 
 /* -------------------------------------------------------------------------- */
 /*                                 REPLY CODES                                 */
@@ -61,6 +79,16 @@ const std::string RPL_ISON_MSG = " :[<nick> {<space><nick>}]\r\n";
 
 const std::string RPL_AWAY = "301";
 const std::string RPL_AWAY_MSG = " <nick> :<away message>\r\n";
+
+const std::string RPL_NOTOPIC = "331";
+const std::string RPL_TOPIC = "332";
+const std::string RPL_INVITING = "341";
+const std::string RPL_NAMREPLY = "353";
+const std::string RPL_ENDOFNAMES = "366";
+const std::string RPL_CHANNELMODEIS = "324";
+const std::string RPL_MOTDSTART = "375";
+const std::string RPL_MOTD = "372";
+const std::string RPL_ENDOFMOTD = "376";
 
 /* ... Tous les autres codes RPL comme RPL_WHOISUSER, RPL_TOPIC, RPL_ENDOFMOTD ... */
 
